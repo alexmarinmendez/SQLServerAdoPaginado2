13ff352154b62c1234ecd258998b91a162cf7f32
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-11-20 10:54:14 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication1/Controllers/ClientesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using WebApplication1.Models;
+using WebApplication1.Dao;
+
+namespace WebApplication1.Controllers
+{
+    public class ClientesController : Controller
+    {
+        ClientesDao clientesDao = new ClientesDao();
+        // GET: Clientes
+        public ActionResult ListadoClientes()
+        {
+            var listado = clientesDao.ListarClientes();
+            return View(listado);
+        }
+    }
+}
